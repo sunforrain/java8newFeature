2b82165cabe42c8fd79d15d8663295bc07955252
sunhl <sunhl@qq.com>
2020-04-24 16:18:40 +0800

    Lambda 基础语法

---
+++ day01/src/com/atguigu/java8/MyFun.java
@@ -0,0 +1,12 @@
+package com.atguigu.java8;
+
+/**
+ * 视频3. 尚硅谷_Java8新特性_Lambda 基础语法
+ * 自定义函数式接口
+ */
+@FunctionalInterface
+public interface MyFun {
+
+	public Integer getValue(Integer num);
+	
+}
